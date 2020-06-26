dabcba919973c050acdf9b587dc37e54f6dff514
Javier Ortiz <31720280+Alastor56@users.noreply.github.com>
2020-06-26 07:57:03 -0500

    Create tabla_tipos.h

---
+++ tabla_tipos.h
@@ -0,0 +1,9 @@
+#ifndef TIPOS_H
+#define TIPOS_H
+
+int max(int t1, int t2);
+void ampliar(char *fin, char *dir, int t1, int t2);
+coid reducir(char *fin, char *dir, int t1, int t2);
+
+void newTemp(char* s);
+#endif
